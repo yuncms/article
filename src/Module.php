ddb8ff93c203fc22efef07a49040b88e1ef02110
Xu Tongle <xutongle@gmail.com>
2018-05-29 10:37:32 +0800

    init

---
+++ src/Module.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\article;
+
+/**
+ * Class Module
+ * @package yuncms\article
+ */
+class Module extends \yii\base\Module
+{
+
+}
